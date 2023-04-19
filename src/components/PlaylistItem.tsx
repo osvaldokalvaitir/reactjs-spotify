637a646288c0b5dc06062141483664d0f611fe2a
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2023-04-19 12:54:05 -0300

    chore: adds typing in new components

--- src/components/PlaylistItem.tsx
+++ src/components/PlaylistItem.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 
-export function PlaylistItem({ title, description, cover_source }) {
+export interface PlaylistItemProps {
+  title: string
+  description: string
+  cover_source: string
+}
+
+export function PlaylistItem({ title, description, cover_source }: PlaylistItemProps) {
   return (
     <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
       <Image src={cover_source} className="w-full" width={120} height={120} alt={title} />
--- src/components/SavedPlaylistItem.tsx
+++ src/components/SavedPlaylistItem.tsx
@@ -1,4 +1,8 @@
-export function SavedPlaylistItem({ title }) {
+export interface SavedPlaylistItemProps {
+  title: string
+}
+
+export function SavedPlaylistItem({ title }: SavedPlaylistItemProps) {
   return (
     <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">{title}</a>
   )
