9791e9ce054250d35b42bca9b82a8a9c499561d5
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2023-04-19 12:50:02 -0300

    feat: adds playlist item component

---
+++ src/components/PlaylistItem.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export function PlaylistItem({ title, description, cover_source }) {
+  return (
+    <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+      <Image src={cover_source} className="w-full" width={120} height={120} alt={title} />
+      <strong className="font-semibold">{title}</strong>
+      <span className="text-sm text-zinc-500">{description}</span>
+    </a>
+  )
+}
